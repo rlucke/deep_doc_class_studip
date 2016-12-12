52c2eddc702479f73fc3f158cc1c5b440cc68e37
rlucke <rlucke@uni-osnabrueck.de>
2016-12-12 13:27:57 +0100

    set document license

--- studip/cli/set_document_license.php
+++ studip/cli/set_document_license.php
@@ -6,18 +6,59 @@
  * Copyright (C) 2016 - Ron Lucke <rlucke@uos.de>
  * 
  * usage: 
- *
+ * -f flag csv file with prediction and document id
+ * -p flag as prediction limit
  */
 require_once 'studip_cli_env.inc.php';
 if (isset($_SERVER["argv"])) {
     // check for command line options
-    $options = getopt("p:");
-    if (isset($options["p"])) {
-        $percentage = $options["p"];
+    $options = getopt("p:f:");
+    if (isset($options["f"])) {
+        $file = $options["f"];
     } else {
-        $percentage = 50;
+        exit(1);
     }
+    if (isset($options["p"])) {
+        $prediction = $options["p"];
+    } else {
+        $prediction = 50;
+    }
+    //read file
+    if (($handle = fopen($file, "r")) !== FALSE) {
+        $documents = array();
+        $header = null;
+        while ($row = fgetcsv($handle)) {
+            if ($header === null) {
+                $header = $row;
+                continue;
+            }
+            $documents[] = array_combine($header, $row);
+        }
+        fclose($handle);
+    }
+    
+    //write in db
     $db = DBManager::get();
+    foreach ($documents as $document) {
+        if ($prediction <= $document["prediction"]) {
+            //TODO set right license id
+             $stmt = $db->prepare("
+                UPDATE
+                    dokumente
+                SET 
+                    protected = '42'
+                WHERE
+                    protected = '2'
+                AND 
+                    dokument_id = :id
+                ");
+            $stmt->bindParam(":id", $document['id']);
+            $stmt->execute();
+            echo $document["id"]." \n";
+        }
+    }
+
+   
    
 }
 ?>
