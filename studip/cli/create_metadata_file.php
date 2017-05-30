8ff83f1db2f4e97916ca5d46652431c028fca662
rlucke <rlucke@uni-osnabrueck.de>
2017-05-30 14:19:16 +0100

    update studip client scripts

--- studip/cli/create_metadata_file.php
+++ studip/cli/create_metadata_file.php
@@ -7,6 +7,7 @@
  * 
  * usage: 
  * -s flag is for semester selection, has to be the name from semester_data table
+ * -f flag is for seminar selection with a csv file. Use a path to a csv file with seminar ids labeled as id
  * to create a metadata file just pipe the output of this script to a file of your choice
  *
  */
@@ -14,11 +15,36 @@ require_once 'studip_cli_env.inc.php';
 if (isset($_SERVER["argv"])) {
     $db = DBManager::get();
     // check for command line options
-    $options = getopt("s:");
+    $options = getopt("s:f:h");
+    if (isset($options["h"])) {
+        echo "\n";
+        echo "\033[1;34mCreate metadata file for deep doc class \033[0m \n";
+        echo "\033[1;34m__________________________________________________________\033[0m \n";
+        echo "\n";
+        echo "\033[1;34musage: \033[0m \n";
+        echo "\n";
+        echo "\033[1;34m-s\033[0m flag is for semester selection, has to be the \n";
+        echo "   name from semester_data table \n";
+        echo "\n";
+        echo "\033[1;34m-f\033[0m flag is for seminar selection with a csv file. \n";
+        echo "   Use a path to a csv file with seminar ids labeled as id \n";
+        echo "\n";
+        echo "to create a metadata file just pipe the output of this  \n";
+        echo "script to a file of your choice \n";
+        echo "\033[1;34m__________________________________________________________ \033[0m \n";
+        echo "\n";
+        exit(1);
+    }
+    
     if (isset($options["s"])) {
         $semester_name = $options["s"];
     } else {
         $semester_name = "";
+    }
+    if (isset($options["f"])) {
+        $file_path = $options["f"];
+    } else {
+        $file_path = "";
     }
 
     if (!empty($semester_name)) {
@@ -35,37 +61,68 @@ if (isset($_SERVER["argv"])) {
         $semester = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         if(empty($semester)) {
-            echo "semester not found";
+            echo "Error: \n";
+            echo "semester not found! \n";
+            echo "please select another semester \n";
             exit(1);
         }
 
         $beginn = $semester[0]["beginn"];
         $ende = $semester[0]["ende"];
 
-        $select_seminar ="
-                SELECT
-                    Seminar_id
-                FROM 
-                    seminare
-                WHERE
+        $sem_seminar ="
                     start_time >= $beginn
                     AND
                     start_time <= $ende
                 ";
         
-    } else {
-        $select_seminar ="
-                SELECT
-                    Seminar_id
-                FROM 
-                    seminare
-                ";
-       
+    } 
+
+    if (!empty($file_path)) {
+        if (($handle = fopen($file_path, "r")) !== FALSE) {
+            $seminars = [];
+            $csv = array_map('str_getcsv', file($file_path));
+            $key = array_search("id" , $csv[0]);
+            if ($key) {
+                foreach ($csv as $num => $row) {
+                    if ($num == 0) { continue; }
+                    array_push($seminars, $row[$key]);
+                }
+                $file_seminar = "Seminar_id IN ("."'".implode("','", $seminars)."'".")";
+            } else {
+                echo "Error: \n";
+                echo "row 'id' not found! \n";
+                echo "please select a csv file with seminar ids labeled as 'id' \n";
+                exit(1);
+            }
+        }
+        else {
+            echo "Error: \n";
+            echo "file not found! \n";
+            echo "please select a csv file with seminar ids labeled as 'id' \n";
+            echo "\n";
+            exit(1);
+        }
+
+    }
+
+    $select_seminar = "SELECT Seminar_id FROM seminare";
+
+    if (!empty($file_seminar) && !empty($sem_seminar)) {
+            $select_seminar .= " WHERE ".$file_seminar . " OR " . $sem_seminar;
+    }
+
+    if (empty($file_seminar) && !empty($sem_seminar)) {
+            $select_seminar .= " WHERE ".$sem_seminar;
+    }
+
+    if (!empty($file_seminar) && empty($sem_seminar)) {
+            $select_seminar .= " WHERE ".$file_seminar;
     }
 
     $stmt = $db->prepare("
             SELECT
-                dokumente.dokument_id as document_id , dokumente.filename as filename, folder.name as folder_name, folder.description as folder_description, dokumente.description as description
+                dokumente.dokument_id as document_id , dokumente.filename as filename, folder.name as folder_name, folder.description as folder_description, dokumente.description as description, dokumente.seminar_id
             FROM 
                 dokumente
             JOIN
@@ -80,7 +137,7 @@ if (isset($_SERVER["argv"])) {
     $stmt->execute();
     $dokumente = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $csv = '"document_id", "filename", "folder_name", "folder_description", "description"'." \n";
+    $csv = '"document_id", "filename", "folder_name", "folder_description", "description", "id"'." \n";
     foreach($dokumente as $dokument) {
         $csv .= '"'.join('","', $dokument).'"'." \n";
     }
--- studip/cli/set_document_license.php
+++ studip/cli/set_document_license.php
@@ -7,13 +7,27 @@
  * 
  * usage: 
  * -f flag csv file with prediction and document id
+ * -l flag license id default is 42
  * -p flag as prediction limit
- * output can be piped to a csv file
  */
 require_once 'studip_cli_env.inc.php';
 if (isset($_SERVER["argv"])) {
     // check for command line options
-    $options = getopt("p:f:");
+    $options = getopt("p:f:l:h");
+    if (isset($options["h"])) {
+        echo "\n";
+        echo "\033[1;34mSet document license with predictions from deep doc class \033[0m \n";
+        echo "\033[1;34m__________________________________________________________ \033[0m \n";
+        echo "\n";
+        echo "\033[1;34m usage: \033[0m \n";
+        echo "\n";
+        echo "\033[1;34m -f\033[0m flag csv file with prediction and document id \n";
+        echo "\033[1;34m -l\033[0m flag license id default is 42\n";
+        echo "\033[1;34m -p\033[0m flag as prediction limit \n";
+        echo "\033[1;34m__________________________________________________________ \033[0m \n";
+        echo "\n";
+        exit(1);
+    }
     if (isset($options["f"])) {
         $file = $options["f"];
     } else {
@@ -24,42 +38,81 @@ if (isset($_SERVER["argv"])) {
     } else {
         $prediction = 50;
     }
+    if (isset($options["l"])) {
+        $license = $options["l"];
+    } else {
+        // TODO set correct default
+        $license = 42;
+    }
     //read file
-    if (($handle = fopen($file, "r")) !== FALSE) {
-        $documents = array();
-        $header = null;
-        while ($row = fgetcsv($handle)) {
-            if ($header === null) {
-                $header = $row;
-                continue;
+
+    if (!empty($file)) {
+        echo "\n";
+        echo "\033[1;34mSet document license with predictions from deep doc class \033[0m \n";
+        echo "\033[1;34m__________________________________________________________ \033[0m \n";
+        echo "\n";
+        if (($handle = fopen($file, "r")) !== FALSE) {
+            echo "\033[1;34mreading file... \033[0m";
+            $documents = [];
+            $csv = array_map('str_getcsv', file($file));
+            $document_id = array_search("document_id" , $csv[0]);
+            $prediction = array_search("prediction" , $csv[0]);
+            if (is_int($document_id) && is_int($prediction)) {
+                foreach ($csv as $num => $row) {
+                    if ($num == 0) { continue; }
+                    array_push($documents, array("id"=>$row[$document_id], "prediction"=> $row[$prediction]));
+                }
+            echo "\033[1;32mdone! \033[0m \n";
+            } else {
+                echo "\033[1;31mfail!\n\n";
+                if (!is_int($document_id)) echo "could not found document_id \n";
+                if (!is_int($prediction)) echo "could not found prediction \n";
+                echo "\033[0m \n";
+                exit(1);
             }
-            $documents[] = array_combine($header, $row);
         }
-        fclose($handle);
+        else {
+            echo "\033[1;31m\nError: file not found! \n";
+            echo "please select a csv file with document ids and predictions \n";
+            echo "\033[0m \n";
+            exit(1);
+        }
+
     }
     
     //write in db
-    
+    echo "\033[1;34m\nwrite in DB... \033[0m \n \n";
     $db = DBManager::get();
-    echo '"document_id", "records affected"'." \n";
+    echo "\033[44m          document_id             record updated \033[0m\n";
     foreach ($documents as $document) {
         if ($prediction <= $document["prediction"]) {
-            //TODO set right license id
              $stmt = $db->prepare("
                 UPDATE
                     dokumente
                 SET 
-                    protected = '42'
+                    protected = :license
                 WHERE
                     protected = '0'
                 AND 
                     dokument_id = :id
                 ");
             $stmt->bindParam(":id", $document['id']);
-            $stmt->execute();
-            echo '"'.$document["id"].'", "'.$stmt->rowCount().'"'." \n";
+            $stmt->bindParam(":license", $license);
+            $exec = $stmt->execute();
+            if($exec) {
+                echo "\033[1;34m".$document["id"]." \033[0m";
+                if ($stmt->rowCount() == 0) {
+                    echo "\033[1;41m       no       \033[0m\n";
+                } else {
+                    echo "\033[1;42m       yes      \033[0m\n";
+                }
+            } else {
+                echo "\033[1;31m Error can not update licence for document:". $document["id"] ."\033[0m \n";
+            }
         }
     }
+    echo "\n";
+    echo "\033[1;32mdone! \033[0m \n \n";
 
    
    
